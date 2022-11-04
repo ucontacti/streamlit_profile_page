9afd7b63a04cec72e99c9759ee02d7b098298c29
ucontacti <ucontacti2012@gmail.com>
2022-11-04 13:55:20 +0100

    more plot and more changes to paging

--- Homepage.py
+++ Homepage.py
@@ -1,16 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Saleh Daghigh Portfolio"
+    page_title="Streamlit Gallery"
 )
 
-st.title("Saleh Daghigh")
-
-# if "my_input" not in st.session_state:
-#     st.session_state["my_input"] = ""
-
-# my_input = st.text_input("Input a text here", st.session_state["my_input"])
-# submit = st.button("Submit")
-# if submit:
-#     st.session_state["my_input"] = my_input
-#     st.write("You have entered: ", my_input)
+st.markdown('##### I have put together a collection of Streamlit Projects Gallery')
--- pages/Twitter Sentiment Analysis.py
+++ pages/Twitter Sentiment Analysis.py
@@ -10,7 +10,8 @@ from sentiment_analyzer import text_cleaner, lemmatize_text, \
 								getPolarity, getPolAnalysis, \
 								getSubjectivity, getSubAnalysis
 
-st.title("Tweeter hot topic Sentimental Analysis")
+st.markdown('### Twitter hot topic Sentimental Analysis')
+
 
 
 auth = tweepy.OAuthHandler(tweepy_token.API_KEY, tweepy_token.API_SECRET)
@@ -29,13 +30,16 @@ for loc in locations:
 
 if 'country' not in st.session_state or \
 	'topics' not in st.session_state or \
-	'trends_json' not in st.session_state:
+	'trends_json' not in st.session_state or\
+	'df_not_exists' not in st.session_state:
 	st.session_state['country'] = ""
 	st.session_state['topics'] = []
 	st.session_state['trends_json'] = {}
+	st.session_state['df_not_exists'] = True
 
 def get_trending_topic(loc_woeid_dict):
 	st.session_state['trends_json'] = json.loads(json.dumps(api.get_place_trends(loc_woeid_dict[st.session_state['country']]), indent=1))
+	st.session_state['df_not_exists'] = True
 
 st.session_state['country_new'] = st.session_state['country']
 st.session_state['country'] = st.selectbox(
@@ -55,40 +59,53 @@ if st.session_state['trends_json'] != {}:
 		trends_topic.append(trend["name"].strip("#"))
 
 st.session_state['topics'] = st.multiselect(
-	'Choose one of the topics:',
+	'Choose one or more topics (additional topics will restrict your query not extend):',
 	trends_topic
 	)
 
 # st.write('You selected:', st.session_state['topics'])
 
-query = f"{' '.join(st.session_state['topics'])} -is:retweet"
+query = f"{' '.join(st.session_state['topics'])} lang:en -is:retweet"
 
 if st.button('Press to Query'):
 	response = client.search_recent_tweets(query=query, max_results=50, tweet_fields=['text', 'lang', 'geo'])
 	tweets_data = response.json()['data']
 	st.session_state.tweet_df = pd.json_normalize(tweets_data) 
-	st.dataframe(st.session_state.tweet_df)
+	with st.expander("DataFrame ⤵️"):
+		st.dataframe(st.session_state.tweet_df)
+	st.session_state['df_not_exists'] = False
 	# st.session_state.tweet_df.to_csv('tweets.csv')
 
-if st.button('Press to apply sentiment analysis'):
+if st.button('Press to apply sentiment analysis', disabled=st.session_state['df_not_exists']):
 	st.session_state.tweet_df['clean_text'] = st.session_state.tweet_df['text'].apply(lemmatize_text)
 	st.session_state.tweet_df["polarity"] = st.session_state.tweet_df["clean_text"].apply(getPolarity)
 	st.session_state.tweet_df["sentiment"] = st.session_state.tweet_df["polarity"].apply(getPolAnalysis)
 	st.session_state.tweet_df["subjectivity"] = st.session_state.tweet_df["clean_text"].apply(getSubjectivity)
 	st.session_state.tweet_df["sub_obj"] = st.session_state.tweet_df["subjectivity"].apply(getSubAnalysis)
-	st.dataframe(st.session_state.tweet_df)
+	# st.dataframe(st.session_state.tweet_df)
 
-if st.button('plot piechart'):
-	fig1, ax1 = plt.subplots()
-	sent_percentage = st.session_state.tweet_df['sentiment'].value_counts()/sum(st.session_state.tweet_df['sentiment'].value_counts())
-	labels = sent_percentage.keys()
-	sizes = sent_percentage.values
-	ax1.pie(sizes, labels=labels, autopct='%1.1f%%')
-	ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+	col1, col2 = st.columns(2)
+	with col1:
+		fig1, ax1 = plt.subplots()
+		sent_percentage = st.session_state.tweet_df['sentiment'].value_counts()/sum(st.session_state.tweet_df['sentiment'].value_counts())
+		labels = sent_percentage.keys()
+		sizes = sent_percentage.values
+		ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+		ax1.axis('equal') 
+		plt.title('Sentiment Pie Chart')
+		
+		st.pyplot(fig1)
 
-	st.pyplot(fig1)
-
+	with col2:
+		fig1, ax1 = plt.subplots()
+		sent_percentage = st.session_state.tweet_df['sub_obj'].value_counts()/sum(st.session_state.tweet_df['sub_obj'].value_counts())
+		labels = sent_percentage.keys()
+		sizes = sent_percentage.values
+		ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+		ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+		plt.title('Subjectivity Pie Chart')
+		
+		st.pyplot(fig1)
 # TODO: add code comment
-# TODO: add second plot
 # TODO: make second for pure text
 # TODO: add better readme
